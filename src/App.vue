2e147dd5b48262d687b61809cbc83e4b641fcce3
Gabriel Unterholzer <unterholzer.gabriel@gmail.com>
2020-10-22 16:55:23 +0200

    Added login form

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,15 @@
   >
     <div class="jumbotron">
       <h1 class="display-4">OwnYourData DataBud</h1>
-      <p class="lead">DataBud is loading <spinner></spinner></p>
+      <p class="lead">
+        DataBud is loading <spinner></spinner>
+      </p>
     </div>
   </div>
+  <login
+    v-else-if="isLoginFormShowed"
+    @login="logIn"
+  ></login>
   <router-view v-else></router-view>
 </template>
 
@@ -15,32 +21,56 @@
 import Vue from "vue";
 import { initialize } from './services';
 import Spinner from './components/Spinner.vue'
+import Login, { Data as LoginData } from './components/Login.vue'
 import { ConfigService } from './services/config-service';
 
 interface IData {
   isInitializing: boolean,
+  isLoggedIn: boolean,
 }
 
 export default Vue.extend({
   components: {
     Spinner,
+    Login,
   },
   created() {
     this.initialize();
   },
   data: (): IData => ({
     isInitializing: true,
+    isLoggedIn: false,
   }),
   methods: {
     async initialize() {
-      const endpoint = ConfigService.get('endpoint', 'url');
       const appKey = ConfigService.get('endpoint', 'credentials', 'appKey');
       const appSecret = ConfigService.get('endpoint', 'credentials', 'appSecret');
 
-      await initialize('', endpoint, appKey, appSecret);
+      this.tryInitializeVaultifier(appKey, appSecret)
+    },
+    async tryInitializeVaultifier(appKey: string, appSecret: string) {
+      this.isInitializing = true;
+
+      try {
+        const endpoint = ConfigService.get('endpoint', 'url');
+
+        const vaultifier = await initialize('', endpoint, appKey, appSecret);
+        this.isLoggedIn = vaultifier.isValid();
+      }
+      catch {
+        /* if there is no data provided */
+      }
 
       this.isInitializing = false;
     },
+    logIn({ appKey, appSecret }: LoginData) {
+      this.tryInitializeVaultifier(appKey, appSecret);
+    }
+  },
+  computed: {
+    isLoginFormShowed(): boolean {
+      return !this.isInitializing && !this.isLoggedIn;
+    }
   }
 });
 </script>
---
+++ src/components/Login.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container">
+    <div class="jumbotron">
+      <h1 class="display-4">OwnYourData DataBud Login</h1>
+
+      <form @submit="submitForm">
+        <div class="form-group">
+          <label>App Key:
+            <input
+              class="form-control"
+              type="text"
+              v-model="appKey"
+            /></label>
+        </div>
+        <div class="form-group">
+          <label>App Secret:
+            <input
+              class="form-control"
+              type="password"
+              v-model="appSecret"
+            /></label>
+        </div>
+
+        <button
+          type="submit"
+          class="btn btn-primary"
+        >Login</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export interface Data {
+  appKey: string,
+  appSecret: string,
+}
+
+import Vue from 'vue'
+export default Vue.extend({
+  data: () => ({
+    appKey: '',
+    appSecret: '',
+  }),
+  methods: {
+    submitForm(event: Event) {
+      event.preventDefault();
+
+      this.$emit('login', {
+        appKey: this.appKey,
+        appSecret: this.appSecret,
+      } as Data);
+    }
+  }
+})
+</script>
