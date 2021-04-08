ca5b13b2f03b717607fe5cde2f075f52b8e8e177
Gabriel Unterholzer <unterholzer.gabriel@gmail.com>
2021-04-08 16:17:35 +0000

    Fixed authentication bug

--- src/App.vue
+++ src/App.vue
@@ -95,7 +95,7 @@ export default Vue.extend({
       try {
         this.vaultSupport = await vaultifier.getVaultSupport();
 
-        if (vaultifier.isAuthenticated()) {
+        if (await vaultifier.isAuthenticated()) {
           this.isLoggedIn = true;
         }
         else {
