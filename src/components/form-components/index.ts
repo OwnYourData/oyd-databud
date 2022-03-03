4553dd69bc464737c74eec6242adf00968b4c3ee
Gabriel Unterholzer <unterholzer.gabriel@gmail.com>
2022-03-03 12:45:56 +0100

    Fixed import

--- src/components/form-components/index.ts
+++ src/components/form-components/index.ts
@@ -1,6 +1,6 @@
 import { entry as inputControlRenderer } from './InputControlRenderer.vue';
 import { entry as dateControlRenderer } from './DateControlRenderer.vue';
-import { entry as multiSelectControlRenderer } from './MultiSelectControlRenderer.vue';
+import { entry as multiSelectControlRenderer } from './SelectControlRenderer.vue';
 
 export const formRenderers = [
   inputControlRenderer,
