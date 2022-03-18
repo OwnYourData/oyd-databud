5d3c3797174a58e3bf94226a2eaaef52db301f9a
Gabriel Unterholzer <unterholzer.gabriel@gmail.com>
2022-03-18 09:15:11 +0100

    Improved date input handling

--- src/components/form-components/DateControlRenderer.vue
+++ src/components/form-components/DateControlRenderer.vue
@@ -3,12 +3,38 @@
     :label="control.label"
     :description="control.uischema.description"
   >
-    <b-form-datepicker
-      :id="control.id + '-input'"
-      :disabled="!control.enabled"
-      v-model="control.data"
-      @input="onChange"
-    />
+    <b-input-group class="mb-3">
+      <b-form-input
+        :id="control.id + '-input'"
+        :disabled="!control.enabled"
+        :required="control.required"
+        :state="state"
+        v-model="control.data"
+        type="text"
+        placeholder="YYYY-MM-DD"
+        autocomplete="off"
+        ref="input"
+        @input="onChange"
+      ></b-form-input>
+      <b-input-group-append>
+        <b-form-datepicker
+          :disabled="!control.enabled"
+          :required="control.required"
+          v-model="control.data"
+          @input="onChange"
+          locale="en"
+          button-only
+          right
+          today-button
+          reset-button
+          close-button
+        />
+      </b-input-group-append>
+    </b-input-group>
+
+    <b-form-invalid-feedback>
+      Input could not be validated.
+    </b-form-invalid-feedback>
   </b-form-group>
 </template>
 
@@ -19,7 +45,7 @@ import {
   rankWith,
   isDateControl,
 } from '@jsonforms/core';
-import { defineComponent } from '@vue/composition-api';
+import { defineComponent, ref } from '@vue/composition-api';
 import { rendererProps, useJsonFormsControl, RendererProps } from '@jsonforms/vue2';
 
 const DateControlRenderer = defineComponent({
@@ -27,15 +53,36 @@ const DateControlRenderer = defineComponent({
   props: {
     ...rendererProps(),
   },
+  data: () => ({
+    state: false,
+  }),
   setup(props: RendererProps<ControlElement>) {
-    return useJsonFormsControl(props);
+    const input = ref(null);
+
+    return {
+      ...useJsonFormsControl(props),
+      input,
+    }
+  },
+  mounted() {
+    this.validate();
   },
   methods: {
+    async validate() {
+      // we must await the change in the vue reactivity system
+      // to notice the new value
+      await this.$nextTick();
+      // @ts-expect-error
+      this.state = this.input?.checkValidity() || false;
+
+      // TODO: also check if it is a valid date value
+    },
     onChange(value: string) {
       this.handleChange(
         this.control.path,
         value,
       );
+      this.validate();
     }
   }
 });
